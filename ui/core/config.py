19ae8c9e3111fefcabad0f7231a8bd3ec6ad1d71
zeinabmansouri482-lgtm <zeinabmansouri482@gmail.com>
2026-01-08 10:22:32 +0400

    UI Working: Game launches with shop cards and basic interactions

--- ui/core/config.py
+++ ui/core/config.py
@@ -1,3 +1,22 @@
 # config.py
 # Hearthstone Clone Project
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 800
+FPS = 60
+
+CARD_WIDTH = 110
+CARD_HEIGHT = 150
+GAP = 15
+
+COLORS = {
+    "BACKGROUND": (25, 20, 30),
+    "SHOP_BG": (60, 40, 30),
+    "BOARD_BG": (40, 50, 60),
+    "HAND_BG": (20, 20, 20),
+    "TEXT_GOLD": (255, 215, 0),
+    "TEXT_WHITE": (255, 255, 255),
+    "CARD_BORDER": (180, 180, 180),
+    "CARD_EMPTY": (50, 50, 50)
+}
+
 
