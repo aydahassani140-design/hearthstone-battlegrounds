b6f988e35872c18874e2678b42bf158c255ffe52
zeinabmansouri482-lgtm <zeinabmansouri482@gmail.com>
2026-02-06 01:21:06 +0400

    Add minions database and base models

---
+++ bgknowhow-main/src/models.py
@@ -0,0 +1,25 @@
+# src/models.py
+import pygame # برای داشتن Rect در مدل
+
+class Minion:
+    def __init__(self, data):
+        self.name = data.get("name", "Unknown")
+        self.attack = data.get("attack", 0)
+        self.health = data.get("health", 0)
+        self.keywords = data.get("keywords", [])
+        self.cost = 3
+        self.is_frozen = False
+        self.rect = pygame.Rect(0, 0, 0, 0) # UI این را بعدا پر می‌کند
+
+class Player:
+    def __init__(self, name="Sylvanas"):
+        self.name = name
+        self.hp = 40
+        self.turn = 1
+        self.gold = 3
+        self.max_gold = 3
+        self.tavern_tier = 1
+        self.upgrade_cost = 5
+        self.hand = []   # List of Minion objects
+        self.board = []  # List of Minion objects
+        self.shop = []   # List of Minion objects
